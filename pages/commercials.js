4b036adf652eb99b932e048a06e06d03e1e3b2fc
yiriYu <davidgomezdit1980@gmail.com>
2025-05-06 12:58:41 +0200

    Add files via upload

---
+++ pages/commercials.js
@@ -0,0 +1,8 @@
+export default function Commercials() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Commercials Page</h1>
+      <p>Aquí irían los vídeos comerciales.</p>
+    </div>
+  )
+}
