84e46334b1dd19e9f340f0e4b0418ae788a8a573
yiriYu <davidgomezdit1980@gmail.com>
2025-05-06 14:05:14 +0200

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>About</h1>
+      <p>David Gómez Ojeda is a freelance Digital Imaging Technician.</p>
+    </div>
+  )
+}
---
+++ pages/feature.js
@@ -0,0 +1,8 @@
+export default function Feature() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Feature Page</h1>
+      <p>Aquí irían los vídeos de ficción.</p>
+    </div>
+  )
+}
