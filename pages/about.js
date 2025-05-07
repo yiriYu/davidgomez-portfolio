ad56e1fa050a0130b5f72420888e5235b656ce54
yiriYu <davidgomezdit1980@gmail.com>
2025-05-07 11:33:42 +0200

    Delete pages directory

--- pages/about.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div style={{ padding: '40px' }}>
-      <h1>About</h1>
-      <p>David Gómez Ojeda is a freelance Digital Imaging Technician.</p>
-    </div>
-  )
-}
