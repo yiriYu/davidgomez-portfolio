23a832b4f264e53ca093fa9efdc9ef5f78c92a48
yiriYu <davidgomezdit1980@gmail.com>
2025-05-07 11:35:32 +0200

    Add files via upload

---
+++ pages/commercials.js
@@ -0,0 +1,165 @@
+
+export default function Commercials() {
+  return (
+    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+      <h1 style={ fontSize: '32px' }>Commercials</h1>
+      <div style={ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gap: '20px' }>
+        
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/4fKUnhAIVp0" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Disney “The last verse”</strong><br />
+            Director: Henry Alex Rubin<br />
+            DOP: Mathieu Plainfosse<br />
+            Prod Co: Tickle Film
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/fQ9vubEsND4" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Youtube “What a time to be a sports fan”</strong><br />
+            Director: Matias & Mathias<br />
+            DOP: Mauro Chiarello<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/2xdXKNQYN2o" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Nike Football “Awaken your madness”</strong><br />
+            Director: Sam Pilling<br />
+            DOP: Norm Li<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://player.vimeo.com/video/956438949" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Amazon Ads “Media Envy”</strong><br />
+            Director: Sam Pilling<br />
+            DOP: Daniel Voldheim<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://player.vimeo.com/video/1056187253" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>PUMA “Year of Sport”</strong><br />
+            Director: Ryan Chun<br />
+            DOP: Joao de Botelho<br />
+            Prod Co: Falca
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://player.vimeo.com/video/939540217" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Uber - Mobility</strong><br />
+            Director: Augusto Papilla<br />
+            DOP: Octavio Arias<br />
+            Prod Co: Pueblo Films
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/00JAcrLKLMU" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Renault 5 “Be the first”</strong><br />
+            Director: Man vs Machine<br />
+            DOP: Daniel Bouquet<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/Ewzvf3dkl4Q" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>HERMES “Barenia”</strong><br />
+            Director: James Gray<br />
+            DOP: Daniel Bouquet<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://player.vimeo.com/video/1072448845" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Hyundai</strong><br />
+            Director: Julien & Quentin<br />
+            DOP: Frederick Backar<br />
+            Prod Co: Mammateam
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/AMHMU0YPFEc" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Jardiance “Little Pill, big story”</strong><br />
+            Director: Declan Whitebloom<br />
+            DOP: <br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+        <div>
+          <div style={ position: 'relative', paddingBottom: '56.25%', height: 0 }>
+            <iframe src="https://www.youtube.com/embed/r08aKGGHAUQ" frameBorder="0"
+              style={ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }
+              allowFullScreen></iframe>
+          </div>
+          <p style={ fontSize: '14px', lineHeight: '1.4', marginTop: '10px' }>
+            <strong>Expedia “One Key Traveler”</strong><br />
+            Director: Kim Gehrig<br />
+            DOP: Emmanuel “Chivo” Lubezki<br />
+            Prod Co: TwentyfourSeven
+          </p>
+        </div>
+    
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,58 @@
+
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: 'white',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'relative',
+    }}>
+      <div style={{ position: 'relative', width: '300px', height: '260px' }}>
+        <img
+          src="/logo.png"
+          alt="Logo"
+          style={{
+            width: '180px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 2
+          }}
+        />
+        <Link href="/commercials">
+          <div style={circleStyle({ top: 0, left: 0 })}>Commercials</div>
+        </Link>
+        <Link href="/feature">
+          <div style={circleStyle({ top: 0, right: 0 })}>Feature</div>
+        </Link>
+        <Link href="/about">
+          <div style={circleStyle({ bottom: 0, left: '50%', transform: 'translateX(-50%)' })}>About</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function circleStyle(positioning) {
+  return {
+    position: 'absolute',
+    width: '110px',
+    height: '110px',
+    borderRadius: '50%',
+    backgroundColor: 'black',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '14px',
+    ...positioning
+  };
+}
